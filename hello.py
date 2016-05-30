6e248fd17d486ab05833eb52051b19a50ed02aea
KangKang <kangkang@KangKangMacBook-Pro.local>
2016-05-30 16:00:19 +0800

    chapter-3

--- hello.py
+++ hello.py
@@ -1,16 +1,32 @@
 from flask import Flask, request, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
+#home目录路由
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    return '<h1>Hello World!</h1> <h2>Your browser is %s</h2>' % request.headers
+    return render_template('index.html',user_agent=user_agent,current_time=datetime.utcnow())
 
+#用户页面路由
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html',name=name)
+
+#错误页面路由
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+@app.errorhandler(500)
+def intenal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     manager.run()
