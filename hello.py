2aff139a5cd39d610dabfa2e345281720ae9049d
KangKang <kangkang@KangKangMacBook-Pro.local>
2016-05-31 20:03:04 +0800

    Chapter-5d

--- hello.py
+++ hello.py
@@ -1,22 +1,60 @@
+import os
 from flask import Flask, request, render_template, session, redirect, url_for, flash
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+#web 表单
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+#database
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate,MigrateCommand
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 #web form class
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
-app = Flask(__name__)
-manager = Manager(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-app.config['SECRET_KEY'] = 'hard to guess string'
+#SQLAlchemy classes
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    def __repr__(self):
+        return '<Role %r>' % self.name
+    #relationship
+    user = db.relationship('User', backref='role')
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    def __repr__(self):
+        return '<User %r>' % self.username
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+#配置shell
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
 
 #home目录路由
 @app.route('/', methods=['GET','POST'])
@@ -24,13 +62,22 @@ def index():
     user_agent = request.headers.get('User-Agent')
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html',name=session.get('name'),form=form,user_agent=user_agent,current_time=datetime.utcnow())
+        form.name.data = ''
 
+        #if old_name is not None and old_name != form.name.data:
+        #    flash('Looks like you have changed your name!')
+ 
+        return redirect(url_for('index'))
+    return render_template('index.html',name=session.get('name'),form=form,user_agent=user_agent,current_time=datetime.utcnow(),known = session.get('known',False))
+    
 #用户页面路由
 @app.route('/user/<name>')
 def user(name):
