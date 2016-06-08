07d57cf1616e78dd44b24fb85363c336bab6a58f
KangKang <kangkang@KangKangMacBook-Pro.local>
2016-06-08 17:20:27 +0800

    10d Gravatar-hash

--- app/models.py
+++ app/models.py
@@ -70,15 +70,22 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
-    # 构造时赋予角色
     def __init__(self, **kwargs):
+        """
+        构造时赋予角色,和头像hash
+        :param kwargs:
+        """
         super(User, self).__init__(**kwargs)
         if self.role is None:
             if self.email == current_app.config['FLASKY_ADMIN']:
                 self.role = Role.query.filter_by(permission=0xff).first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+        # 缓存头像hash
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
 
     # 刷新新用户最后访问时间
     def ping(self):
@@ -153,7 +160,7 @@ class User(UserMixin, db.Model):
             url = 'http://secure.gravatar.com/avatar'
         else:
             url = 'http://gravatar.com/avatar'
-        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating)
 
